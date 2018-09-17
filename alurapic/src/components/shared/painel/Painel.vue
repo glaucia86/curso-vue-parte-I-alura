4dc0c827f4db5e15b9a1fedf3dbc54014ea91fa9
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-17 19:56:05 -0300

    Alteração no código para colocar box-shadow na aplicação.

--- alurapic/src/components/shared/painel/Painel.vue
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -12,7 +12,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .painel {
   padding: 0 auto;
   border: solid 2px grey;
@@ -32,6 +32,10 @@ export default {
   margin: 0 0 15px 0;
   padding: 10px;
   text-transform: uppercase;
+}
+
+* {
+  box-shadow: 5px 5px 5px;
 }
 </style>
 
