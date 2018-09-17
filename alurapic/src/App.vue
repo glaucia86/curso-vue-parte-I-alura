8750baf76365c170fdd4ea2f588b84860c5effaf
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-17 18:40:17 -0300

    Alteração no design da aplicação.

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>    
+    <h1 class="titulo">{{ titulo }}</h1>
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto in fotos">
-        <img :src="foto.url" :alt="foto.titulo"/>
+
+        <div class="painel">
+          <h2 class="painel-titulo">{{foto.titulo}}</h2>
+          <div class="painel-corpo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+          </div>
+        </div>        
       </li>
     </ul>
   </div>
@@ -28,7 +34,7 @@ export default {
 </script>
 
 <style>
-.centralizado {
+.titulo {
   text-align: center;
 }
 
@@ -44,5 +50,32 @@ export default {
 
 .lista-fotos .lista-fotos-item {
   display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+/* estilo do painel */
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
 }
 </style>
