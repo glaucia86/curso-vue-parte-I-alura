5fc1c19322b641f6e7b050b4b70e74c86bbece23
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-17 18:09:47 -0300

    Criando estilos na aplicação.

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>    
-    <ul>
-      <li v-for="foto in fotos">
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>    
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos">
         <img :src="foto.url" :alt="foto.titulo"/>
       </li>
     </ul>
@@ -19,12 +19,30 @@ export default {
   },
 
   created() {
-    this.$http.get('http://localhost:3000/v1/fotos')
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
       .then(res => res.json())
-      .then(fotos => this.fotos = fotos);
-  },
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
+  }
 };
 </script>
 
 <style>
+.centralizado {
+  text-align: center;
+}
+
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
 </style>
