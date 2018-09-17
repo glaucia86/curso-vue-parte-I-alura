e5e37f7b44be4a4f707917cb82132d3d95328d46
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-17 19:20:23 -0300

    Criação de componente na aplicação.

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -2,15 +2,12 @@
   <div class="corpo">
     <h1 class="titulo">{{ titulo }}</h1>
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto in fotos">
+      <li class="lista-fotos-item" v-for="foto in fotos"></li>
 
-        <div class="painel">
-          <h2 class="painel-titulo">{{foto.titulo}}</h2>
-          <div class="painel-corpo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
-          </div>
-        </div>        
-      </li>
+      <meu-painel :titulo="foto.titulo">
+        <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+      </meu-painel>
+      
     </ul>
   </div>
 </template>
@@ -54,28 +51,5 @@ export default {
 
 .imagem-responsiva {
   width: 100%;
-}
-
-/* estilo do painel */
-
-.painel {
-  padding: 0 auto;
-  border: solid 2px grey;
-  display: inline-block;
-  margin: 5px;
-  box-shadow: 5px 5px 10px grey;
-  width: 200px;
-  height: 100%;
-  vertical-align: top;
-  text-align: center;
-}
-
-.painel .painel-titulo {
-  text-align: center;
-  border: solid 2px;
-  background: lightblue;
-  margin: 0 0 15px 0;
-  padding: 10px;
-  text-transform: uppercase;
 }
 </style>
---
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="painel">
+        <h2 class="painel-titulo">{{foto.titulo}}</h2>
+            <div class="painel-corpo">
+                
+            </div>
+        </div> 
+</template>
+
+<script>
+
+export default {
+
+};
+
+</script>
+
+<style>
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+</style>
+
