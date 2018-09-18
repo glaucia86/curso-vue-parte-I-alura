541d5537a5d658b2e3264db5cba835059be9acb5
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-18 11:51:26 -0300

    Adicionando a biblioteca 'vue-router'

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
