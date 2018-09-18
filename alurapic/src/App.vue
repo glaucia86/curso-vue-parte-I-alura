285057d13cbb5e5c4b0822264991b86f589fe4d1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-18 02:34:52 -0300

    Aprimoramento do filtro & desenvolvimento da lógica.

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -2,9 +2,8 @@
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
     <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre pelo Título da Foto">
-    {{ filtro }}
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
         <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
@@ -28,6 +27,17 @@ export default {
       fotos: [],
       filtro: ''
     };
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let expressao = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => expressao.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
   },
 
   created() {
@@ -60,5 +70,10 @@ export default {
 
 .imagem-responsiva {
   width: 100%;
+}
+
+.filtro {
+  display: block;
+  width: 100%
 }
 </style>
