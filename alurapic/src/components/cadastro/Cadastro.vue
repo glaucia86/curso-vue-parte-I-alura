83a436205397ee95960eb2a11ea9f5c100b9199f
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-09-18 11:24:21 -0300

    Criação de um novo componente: Cadastro.vue

---
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 class="centralizado">Cadastro</h1>
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+>
+
+<style scoped>
+    .centralizado {
+        text-align: center;
+    }
+</style>
